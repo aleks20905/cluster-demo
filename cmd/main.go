12d0637044b403751764bb2ccc596e47a31eb6de
aleks <aleks_20905@mail.bg>
2024-12-13 21:06:52 +0200

    semicolon error

--- cmd/main.go
+++ cmd/main.go
@@ -25,7 +25,7 @@ func init() {
 	var err error
 
 	dsn := os.Getenv("DATABASE_URL")
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
 	}
